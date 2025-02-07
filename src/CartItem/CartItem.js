fbec83a43a9609c97227942c8dd68b00f8a561d0
Ethann <ethann@shkermit.ch>
2025-02-07 11:13:23 +0100

    feat: implement CartItem class following the CartItem test class

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -7,45 +7,59 @@ const InvalidPriceException = require("./InvalidPriceException.js");
 module.exports = class CartItem {
 
     //region private attributes
+    #articleId;
+    #name;
+    #quantity;
+    #price;
     //endregion private attributes
 
     //region public methods
     constructor(articleId, name, quantity, price) {
-	throw new Error();
+        if (articleId < 1) {
+            throw new InvalidArticleIdException();
+        }
+
+        this.#articleId = articleId;
+        this.#name = name;
+        this.quantity = quantity;
+        this.price = price;
     }
 
     get articleId() {
-        throw new Error();
+        return this.#articleId;
     }
 
     get name() {
-        throw new Error();
+        return this.#name;
     }
 
     get quantity() {
-        throw new Error();
+        return this.#quantity;
     }
 
     set quantity(value) {
-        throw new Error();
+        if (value < 1) {
+            throw new InvalidQuantityException();
+        }
+        this.#quantity = value;
     }
 
     get price() {
-        throw new Error();
+        return this.#price;
     }
 
     set price(value) {
-        throw new Error();
+        if (value < 10) {
+            throw new InvalidPriceException();
+        }
+        this.#price = value;
     }
 
     get total() {
-        throw new Error();
+        return this.#quantity * this.#price;
     }
     //endregion public methods
 
     //region private methods
     //endregion private methods
 }
-
-
-
